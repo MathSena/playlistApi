f6f2a183a439ceb309e27e60ce6f3486f56e4f36
MathSena <MathSena07@hotmail.com>
2023-12-19 00:42:32 +0000

    API REST com Spring Webflux e MongoDB: parte4- criando o controller

---
+++ src/main/java/com/mathsena/playlistapi/controller/PlaylistController.java
@@ -0,0 +1,30 @@
+package com.mathsena.playlistapi.controller;
+
+import com.mathsena.playlistapi.models.Playlist;
+import com.mathsena.playlistapi.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class PlaylistController {
+
+    @Autowired
+    PlaylistService playlistService;
+
+    @GetMapping("/playlists")
+    public Flux<Playlist> getPlaylistNames() {
+        return playlistService.getAllPlaylistNames();
+    }
+
+    @GetMapping("/playlists/{id}")
+    public Mono<Playlist> getPlaylistNameById(@PathVariable String id) {
+        return playlistService.getPlaylistNameById(id);
+    }
+
+    @PostMapping("/playlists")
+    public Mono<Playlist> createPlaylist(@RequestBody Playlist playlist) {
+        return playlistService.createPlaylist(playlist);
+    }
+}
